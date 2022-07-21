d21bf80933b536b8194cec881f48f84b8fa78670
Kimbert Maldonado <kimbert.m.maldonado@gmail.com>
2022-07-21 15:18:01 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Layouts/Header/index";
+function App() {
+  return (
+    <div className="App">
+      <div className="Header">
+        <Header />
+      </div>
+      <div className="Sidebar">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Layouts/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>HEADER</div>
+  )
+}
+
+export default index
