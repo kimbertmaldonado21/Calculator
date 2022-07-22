491a0c78b83c51b95c495126b946bbbb6a9b129a
Kimbert Maldonado <kimbert.m.maldonado@gmail.com>
2022-07-22 22:43:12 +0800

    last edit

--- src/Layouts/Content/index.js
+++ src/Layouts/Content/index.js
@@ -8,8 +8,18 @@ function Index() {
   const ops = ['/','*','+','-','.'];
 
   const updateCalc = value =>{
-    setNum(num, + value);
-    alert(num);
+   if(
+    ops.includes(value) && num === '' ||
+    ops.includes(value) && ops.includes(num.slice(-1))
+   ){
+    return;
+   }
+
+    if(!ops.includes(value)){
+      setResult(eval(num + value).toString());
+    }
+    setNum(num + value);
+
   }
   const makeDigits = () =>{
     const digits =[];
@@ -23,11 +33,24 @@ function Index() {
     }
     return digits;
   }
-
+  const calculate = () =>{
+    setNum(eval(num).toString());
+  }
+  const clear = () =>{
+    setNum("");
+    setResult("");
+  }
+  const deleteLast = () =>{
+    if(num == ''){
+      return;
+    }
+    const value = num.slice(0, -1);
+    setNum(value);
+  }
   return(
     <div className='calculator'>
       <div className='display'>
-        {result ? <span>(0)</span> : ''}
+        {result ? <span>({result})</span> : '(0)'} &nbsp;
          { num  || "0"}
       </div>
       <div className='operators'>
@@ -35,13 +58,14 @@ function Index() {
         <button onClick={()=>updateCalc('/')}>/</button>
         <button onClick={()=>updateCalc('+')}>+</button>
         <button onClick={()=>updateCalc('-')}>-</button>
-        <button>DEL</button>
+        <button onClick={deleteLast}>DEL</button>
+        <button onClick={clear}>C</button>
       </div>
       <div className='digits'>
         {makeDigits()}
         <button onClick={()=>updateCalc('0')}>0</button>
         <button onClick={()=>updateCalc('.')}>.</button>
-        <button>=</button>
+        <button onClick={calculate}>=</button>
 
       </div>
     </div>
